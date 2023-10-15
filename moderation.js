95acdd4d9928d7deb48ba6b0b87fe6f180163f23
OreczX <111639454+OreczXOfficial@users.noreply.github.com>
2023-10-15 17:26:09 -0300

    Update moderation.js

--- moderation.js
+++ moderation.js
@@ -1,21 +1,52 @@
-class HelloWorld {
+class Moderation {
   getInfo() {
     return {
       id: 'moderation',
       name: 'Moderation',
       blocks: [
         {
-          opcode: 'test',
-          blockType: Scratch.BlockType.REPORTER,
-          text: 'Test Text'
-        }
-      ]
+          opcode: 'moderate',
+          blockType: Scratch.BlockType.BOOLEAN,
+          text: 'Moderate [ONE] for Prohibited Words',
+          arguments: {
+            ONE: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'Test Moderation Text',
+            },
+          },
+        },
+      ],
     };
   }
 
-  hello() {
-    return 'World!';
+  moderate(args) {
+    const mensagem = args.ONE.toLowerCase();
+    const url = "https://oreczxofficial.github.io/bad.txt"; // O URL é fixo
+
+    return fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          return response.text();
+        } else {
+          throw new Error('Não foi possível buscar o arquivo.');
+        }
+      })
+      .then((textoDoSite) => {
+        const palavrasProibidas = textoDoSite.toLowerCase().split('\n');
+
+        for (const palavraProibida of palavrasProibidas) {
+          if (mensagem.includes(palavraProibida.trim())) {
+            return false; // Se uma palavra proibida for encontrada, retorna falso.
+          }
+        }
+
+        return true; // Se nenhuma palavra proibida for encontrada, retorna verdadeiro.
+      })
+      .catch((error) => {
+        console.error(error);
+        return false; // Em caso de erro na solicitação, também retorna falso.
+      });
   }
 }
 
-Scratch.extensions.register(new HelloWorld());
+Scratch.extensions.register(new Moderation());
